f19808c18239571a2fe30e14584eeace86d820cb
LENOVO <neerajbhattneeraj@gmail.com>
2019-11-28 15:07:46 +0530

    addd for the change

---
+++ src/app/chat-message/chat-message.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { UsersService } from './../users/users.service';
+import { Message } from './../messages/message.model';
+import { User } from './../users/user.model';
+
+@Component({
+  selector: 'app-chat-message',
+  templateUrl: './chat-message.component.html',
+  styleUrls: ['./chat-message.component.css']
+})
+export class ChatMessageComponent implements OnInit {
+  @Input() message: Message;
+  currentUser: User;
+  incoming: boolean;
+
+  constructor(public usersService: UsersService) {}
+
+  ngOnInit(): void {
+    this.usersService.currentUser.subscribe((user: User) => {
+      this.currentUser = user;
+      if (this.message.author && user) {
+        this.incoming = this.message.author.id !== user.id;
+      }
+    });
+  }
+  getAnswers(){
+    alert('asdfsadf '+this.message.text);
+    // var object;
+    // this.usersService.getJSON().subscribe(data => object=data, error => console.log(error));
+  }
+}
